419698b58c2a4a0e21c07e98da207678b1539f90
Favour Max-Oti <maxotif@gmail.com>
2024-12-27 12:34:12 +0100

    addded dependency for linter

--- apps/jobber-auth/src/main.ts
+++ apps/jobber-auth/src/main.ts
@@ -9,15 +9,21 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const globalPrefix = 'api';
+
   app.setGlobalPrefix(globalPrefix);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     })
   );
+
   const port = process.env.PORT || 3000;
+
   await app.listen(port);
+
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );

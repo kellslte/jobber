1a33e477f33546076e35064ab263ebe0f63ac55b
Favour Max-Oti <maxotif@gmail.com>
2024-12-27 14:00:25 +0100

    fixed issues with the test dependencies

--- apps/jobber-auth/src/app/users/users.resolver.spec.ts
+++ apps/jobber-auth/src/app/users/users.resolver.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersResolver } from './users.resolver';
+import { UsersService } from './users.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('UsersResolver', () => {
   let resolver: UsersResolver;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersResolver],
+      providers: [
+        {
+          provide: PrismaService,
+          useValue: {},
+        },
+        {
+          provide: UsersService,
+          useValue: {},
+        },
+        UsersResolver,
+      ],
     }).compile();
 
     resolver = module.get<UsersResolver>(UsersResolver);
--- apps/jobber-auth/src/app/users/users.service.spec.ts
+++ apps/jobber-auth/src/app/users/users.service.spec.ts
@@ -1,12 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        {
+          provide: PrismaService,
+          useValue: {},
+        },
+        UsersService,
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
